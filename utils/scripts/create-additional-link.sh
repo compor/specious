b3e46263476cff14ccf4290b320a430a96241382
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-07-27 16:30:33 +0100

    simplify link creation utility script

--- utils/scripts/create-additional-link.sh
+++ utils/scripts/create-additional-link.sh
@@ -17,32 +17,30 @@ BMK_DIR=$1
 
 
 if [ -z "$2" ]; then 
-  echo "error: cmake source fragment file was not provided" 
+  echo "error: cmake target fragment file was not provided" 
 
   exit 2
 fi
 
-CMAKE_SOURCE_FRAGMENT_FILE=$2
-
-
-CMAKE_TARGET_FRAGMENT_FILE="CMakeLists-additional.txt"
+CMAKE_FRAGMENT_FILE=$2
 
 
 # print configuration vars
 
 echo "info: printing configuation vars"
 echo "info: benchmark dir: ${BMK_DIR}"
-echo "info: cmake source fragment file: ${CMAKE_SOURCE_FRAGMENT_FILE}"
-echo "info: cmake target fragment file: ${CMAKE_TARGET_FRAGMENT_FILE}"
+echo "info: cmake fragment file: ${CMAKE_FRAGMENT_FILE}"
 echo ""
 
 pushd ${BMK_DIR}
 
-BMKS=$(ls -1 .)
+BMKS=$(ls -1)
 
 for bmk in ${BMKS}; do
+  [ ! -d ${bmk} ] && continue
+
   pushd ${bmk}/src/
-    ln -sf ../../../${CMAKE_SOURCE_FRAGMENT_FILE} ${CMAKE_TARGET_FRAGMENT_FILE}
+    ln -sf ../../../${CMAKE_FRAGMENT_FILE} ${CMAKE_FRAGMENT_FILE}
   popd
 done
 
