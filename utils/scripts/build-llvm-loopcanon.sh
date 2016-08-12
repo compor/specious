f9b75d51a10417faf793dd0a0586a54e6723e863
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-08-12 11:59:39 +0100

    add example build script

---
+++ utils/scripts/build-llvm-loopcanon.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# initialize configuration vars
+
+SRC_DIR=""
+INSTALL_PREFIX=""
+
+
+# set configuration vars
+
+if [ -z "$1" ]; then 
+  echo "error: source directory was not provided" 
+
+  exit 1
+fi
+
+SRC_DIR=$1
+
+if [ -z "$2" ]; then 
+  INSTALL_PREFIX="${SRC_DIR}/../install/"
+else
+  INSTALL_PREFIX="$2"
+fi
+
+
+# print configuration vars
+
+echo "info: printing configuation vars"
+echo "info: source dir: ${SRC_DIR}"
+echo "info: install dir: ${INSTALL_PREFIX}"
+echo ""
+
+
+LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
+LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi" 
+
+CC=clang CXX=clang++ \
+  cmake \
+  -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+  -DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+  -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+  -DUSE_LLVM=On \
+  "${SRC_DIR}"
+
+
+exit $?
+
+
