b1e10605cf495475d0c45c1ae8e879e23b9095d5
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-02-06 11:51:29 +0000

    add cmd arg to allow symlinks to multiple dirs

--- utils/scripts/source_tree/symlink-bmk-src-dir.sh
+++ utils/scripts/source_tree/symlink-bmk-src-dir.sh
@@ -6,21 +6,28 @@ SHOW_HELP=0
 BMK_CONFIG_FILE=""
 BMK_SOURCE_DIR=""
 BMK_TARGET_DIR=""
+BMK_SUBDIRS=""
 
 
 # parse and check cmd line options
 
-CMDOPTS=":c:s:t:h"
+CMDOPTS=":c:s:t:l:h"
+
+CMD_ARGS_DEFAULT=("-l src")
+
+SCRIPT_ARGS=("${@}" "${CMD_ARGS_DEFAULT[@]}")
 
 HELP_STRING="Usage: ${0} OPTIONS
 
 -c file    benchmark config file
 -s dir     benchmark source directory
 -t dir     benchmark target directory
+-l subdir  benchmark subdir (use multiple times for more than one subdirs)
+           default value: src
 -h         help
 "
 
-while getopts $CMDOPTS cmdopt; do
+while getopts ${CMDOPTS} cmdopt "${SCRIPT_ARGS[@]}"; do
   case $cmdopt in
     c)
       BMK_CONFIG_FILE=$OPTARG
@@ -30,6 +37,9 @@ while getopts $CMDOPTS cmdopt; do
       ;;
     t)
       BMK_TARGET_DIR=$OPTARG
+      ;;
+    l)
+      BMK_SUBDIRS=("${BMK_SUBDIRS[@]}" "${OPTARG}")
       ;;
     h)
       SHOW_HELP=1
@@ -77,6 +87,10 @@ echo "info: printing configuration vars"
 echo "info: benchmark config file: ${BMK_CONFIG_FILE}"
 echo "info: benchmark source dir: ${BMK_SOURCE_DIR}"
 echo "info: benchmark target dir: ${BMK_TARGET_DIR}"
+echo -n "info: benchmark subdirs: "
+for BMK_SUBDIR in ${BMK_SUBDIRS[@]}; do 
+  echo -n "${BMK_SUBDIR} "
+done
 echo ""
 
 
