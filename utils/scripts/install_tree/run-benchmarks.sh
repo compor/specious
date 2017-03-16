8e8128c222d218ef589a64f287558db9e7792a9a
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-03-16 11:05:17 +0000

    revamp run script

--- utils/scripts/install_tree/run-benchmarks.sh
+++ utils/scripts/install_tree/run-benchmarks.sh
@@ -1,78 +1,147 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# internal vars
+
+OUTS='/dev/stdout'
+ERRS='/dev/stderr'
+
 
 # initialize configuration vars
 
+SHOW_HELP=0
+BMK_CONFIG_FILE=""
 BMK_INSTALL_DIR=""
-INPUT_TYPE="test"
+BMK_DATA_DIR=""
+BMK_DATA_TYPEBMK_DATA_TYPE=""
 
-BENCHMARKS=(
-  #"400.perlbench"
-  #"401.bzip2"
-  #"403.gcc"
-  #"429.mcf"
-  #"433.milc"
-  #"444.namd"
-  #"445.gobmk"
-  #"447.dealII"
-  #"450.soplex"
-  #"453.povray"
-  #"456.hmmer"
-  #"458.sjeng"
-  #"462.libquantum"
-  #"464.h264ref"
-  "470.lbm"
-  #"471.omnetpp"
-  #"473.astar"
-  #"482.sphinx3"
-  #"483.xalancbmk"
-)
 
-# set configuration vars
+# parse and check cmd line options
 
-if [ -z "$1" ]; then 
-  echo "error: benchmark install directory was not provided" 
+CMDOPTS=":c:i:d:t:qh"
 
-  exit 1
+HELP_STRING="\
+Usage: ${0} OPTIONS
+
+-c file    benchmark config file
+-i dir     benchmark install directory
+-d dir     data set dir
+-t string  data set type
+-q         silent mode (no output)
+-h         help
+"
+
+while getopts ${CMDOPTS} cmdopt "${SCRIPT_ARGS[@]}"; do
+  case $cmdopt in
+    c)
+      BMK_CONFIG_FILE=$OPTARG
+      ;;
+    i)
+      BMK_INSTALL_DIR=$OPTARG
+      ;;
+    d)
+      BMK_DATA_DIR=$OPTARG
+      ;;
+    t)
+      BMK_DATA_TYPE=$OPTARG
+      ;;
+    q)
+      OUTS="/dev/null"
+      ERRS="/dev/null"
+      ;;
+    h)
+      SHOW_HELP=1
+      ;;
+    \?)
+      echo "error: invalid option: -$OPTARG" > $ERRS
+      exit 1
+      ;;
+    :)
+      echo "error: option -$OPTARG requires an argument" > $ERRS
+      exit 1
+      ;;
+  esac
+done
+
+
+if [ "$SHOW_HELP" -ne 0 ]; then
+  echo "$HELP_STRING" > $ERRS
+
+  exit 0
 fi
-
-BMK_INSTALL_DIR=$1
-
-
-if [ -z "$2" ]; then 
-  echo "error: cmake target fragment file was not provided" 
-
-  exit 2
-fi
-
-INPUT_TYPE=$2
 
 
 # print configuration vars
 
-echo "info: printing configuration vars"
-echo "info: benchmark install dir: ${BMK_INSTALL_DIR}"
-echo "info: input type: ${INPUT_TYPE}"
-echo ""
+INFO_STR="\
+info: printing configuration vars
+info: benchmark config file: ${BMK_CONFIG_FILE}
+info: benchmark install dir: ${BMK_INSTALL_DIR}
+info: benchmark data set dir: ${BMK_DATA_DIR}
+info: benchmark data set type: ${BMK_DATA_TYPE}
+"
+
+echo "$INFO_STR" > $OUTS
+
+if [ -z "$BMK_CONFIG_FILE" -o ! -e "$BMK_CONFIG_FILE" ]; then
+  echo "error: benchmark config file was not provided or does not exist" > $ERRS
+
+  exit 1
+fi
+
+if [ -z "$BMK_INSTALL_DIR" -o ! -e "$BMK_INSTALL_DIR" ]; then
+  echo "error: benchmark install dir was not provided or does not exist" > $ERRS
+
+  exit 1
+fi
+
+if [ -z "$BMK_DATA_DIR" -o ! -e "$BMK_DATA_DIR" ]; then
+  echo "error: benchmark data dir was not provided or does not exist" > $ERRS
+
+  exit 1
+fi
+
+if [ -z "$BMK_DATA_TYPE" ]; then
+  echo "error: benchmark data set type was not provided" > $ERRS
+
+  exit 1
+fi
 
 
-for b in ${BENCHMARKS[@]}; do
-  cd $BMK_INSTALL_DIR/${b}
-  SHORT_EXE=${b##*.} # cut off the numbers ###.short_exe
+# operations
 
-  # read the command file
-  IFS=$'\n' read -d '' -r -a commands < ../invocations/${b}.${INPUT_TYPE}.cmd
+# check if out dir location is given in relative form
+if [ "${BMK_INSTALL_DIR}" == "${BMK_INSTALL_DIR#/}" ]; then 
+  BMK_INSTALL_DIR="$(pwd)/${BMK_INSTALL_DIR}"
+fi
 
-  for input in "${commands[@]}"; do
-    if [[ ${input:0:1} != '#' ]]; then # allow us to comment out lines in the cmd files
-      echo "~~~Running ${b}"
-      pushd data/${INPUT_TYPE}/input/
-      echo "  ${RUN} ${SHORT_EXE} ${input}"
-      eval ${RUN} ../../../exe/${SHORT_EXE} ${input}
-      popd
-    fi
-  done
+if [ "${BMK_DATA_DIR}" == "${BMK_DATA_DIR#/}" ]; then 
+  BMK_DATA_DIR="$(pwd)/${BMK_DATA_DIR}"
+fi
+
+readarray BENCHMARKS < ${BMK_CONFIG_FILE}
+
+for BMK in ${BENCHMARKS[@]}; do
+  # trim whitespace
+  BMK=$(echo $BMK | xargs)
+
+  BMK_EXE=${BMK##*.} # cut off the numbers ###.bmk
+
+  #pushd ${BMK_INSTALL_DIR}/CPU2006/${BMK} > $OUTS
+
+  # read command args file
+  BMK_ARGS_FILE=${BMK_DATA_DIR}/invocations/${BMK_DATA_TYPE}/${BMK}.${BMK_DATA_TYPE}.cmd
+  IFS=$'\n' read -d '' -r -a BMK_ARGS < ${BMK_ARGS_FILE}
+
+  echo "running ${BMK}" > $OUTS
+
+  pushd ${BMK_DATA_DIR}/CPU2006/${BMK}/data/${BMK_DATA_TYPE}/input/
+
+  echo "${BMK_EXE} ${BMK_DATA_TYPE}" > $OUTS
+  eval ${BMK_INSTALL_DIR}/CPU2006/${BMK}/exe/${BMK_EXE} ${BMK_ARGS}
+
+  popd
 done
 
-echo ""
-echo "Done!"
+
+exit 0
 
