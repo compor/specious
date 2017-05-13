f74214b43afc5e0d2839ecc3f2e1c4f9da507b82
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-05-13 17:07:22 +0100

    setup basic bitcode generation pipeline

--- cmake/fragments/BasicFragment.cmake
+++ cmake/fragments/BasicFragment.cmake
@@ -4,6 +4,10 @@ if(COMMAND SetupBmkBasicInstall)
   SetupBmkBasicInstall(${PROJECT_NAME})
 endif()
 
+if(COMMAND AttachBasicBitcodeGenPipeline)
+  AttachBasicBitcodeGenPipeline(${PROJECT_NAME})
+endif()
+
 if(COMMAND AttachLoopC14NPipeline)
   AttachLoopC14NPipeline(${PROJECT_NAME})
 endif()
--- cmake/pipelines/BasicBitcodeGen.cmake
+++ cmake/pipelines/BasicBitcodeGen.cmake
@@ -1,14 +1,67 @@
 # cmake file
 
-llvmir_attach_bc_target(${PROJECT_NAME}_bc ${PROJECT_NAME})
-add_dependencies(${PROJECT_NAME}_bc ${PROJECT_NAME})
+# configuration
 
-llvmir_attach_link_target(${PROJECT_NAME}_link
-  ${PROJECT_NAME}_bc)
-add_dependencies(${PROJECT_NAME}_link ${PROJECT_NAME}_bc)
+macro(BasicBitcodeGenPipelineSetup)
+  set(PIPELINE_NAME "BasicBitcodeGen")
+  set(PIPELINE_INSTALL_TARGET "${PIPELINE_NAME}-install")
+endmacro()
 
-llvmir_attach_executable(${PROJECT_NAME}_bc_exe ${PROJECT_NAME}_link)
-add_dependencies(${PROJECT_NAME}_bc_exe ${PROJECT_NAME}_link)
 
-target_link_libraries(${PROJECT_NAME}_bc_exe m)
+function(AttachBasicBitcodeGenPipeline trgt)
+  BasicBitcodeGenPipelineSetup()
+
+  if(NOT TARGET ${PIPELINE_NAME})
+    add_custom_target(${PIPELINE_NAME})
+  endif()
+
+  set(PIPELINE_SUBTARGET "${PIPELINE_NAME}_${trgt}")
+  set(PIPELINE_PREFIX ${PIPELINE_SUBTARGET})
+
+  ## pipeline targets and chaining
+  llvmir_attach_bc_target(${PIPELINE_PREFIX}_bc ${trgt})
+  add_dependencies(${PIPELINE_PREFIX}_bc ${trgt})
+
+  llvmir_attach_link_target(${PIPELINE_PREFIX}_link
+    ${PIPELINE_PREFIX}_bc)
+  add_dependencies(${PIPELINE_PREFIX}_link ${PIPELINE_PREFIX}_bc)
+
+  llvmir_attach_executable(${PIPELINE_PREFIX}_bc_exe ${PIPELINE_PREFIX}_link)
+  add_dependencies(${PIPELINE_PREFIX}_bc_exe ${PIPELINE_PREFIX}_link)
+
+  target_link_libraries(${PIPELINE_PREFIX}_bc_exe m)
+
+
+  # installation
+  get_property(bmk_name TARGET ${trgt} PROPERTY BMK_NAME)
+
+  InstallBasicBitcodeGenPipelineLLVMIR(${PIPELINE_PREFIX}_link ${bmk_name})
+endfunction()
+
+
+function(InstallBasicBitcodeGenPipelineLLVMIR pipeline_part_trgt bmk_name)
+  BasicBitcodeGenPipelineSetup()
+
+  if(NOT TARGET ${PIPELINE_INSTALL_TARGET})
+    add_custom_target(${PIPELINE_INSTALL_TARGET})
+  endif()
+
+  get_property(llvmir_dir TARGET ${pipeline_part_trgt} PROPERTY LLVMIR_DIR)
+
+  # strip trailing slashes
+  string(REGEX REPLACE "(.*[^/]+)(//*)$" "\\1" llvmir_stripped_dir ${llvmir_dir})
+  get_filename_component(llvmir_part_dir ${llvmir_stripped_dir} NAME)
+
+  set(PIPELINE_DEST_SUBDIR
+    ${CMAKE_INSTALL_PREFIX}/CPU2006/${bmk_name}/llvm-ir/${llvmir_part_dir})
+
+  set(PIPELINE_PART_INSTALL_TARGET "${pipeline_part_trgt}-install")
+
+  add_custom_target(${PIPELINE_PART_INSTALL_TARGET}
+    COMMAND ${CMAKE_COMMAND} -E
+    copy_directory ${llvmir_dir} ${PIPELINE_DEST_SUBDIR})
+
+  add_dependencies(${PIPELINE_PART_INSTALL_TARGET} ${pipeline_part_trgt})
+  add_dependencies(${PIPELINE_INSTALL_TARGET} ${PIPELINE_PART_INSTALL_TARGET})
+endfunction()
 
