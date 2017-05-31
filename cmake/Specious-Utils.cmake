b2cd2b65b195a7c192b6f2860d8fb00ed2635d57
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-05-31 20:39:17 +0100

    move utility function to more general place

--- cmake/Specious-Utils.cmake
+++ cmake/Specious-Utils.cmake
@@ -1,5 +1,7 @@
 # cmake file
 
+include(CMakeParseArguments)
+
 function(add_prefix outvar prefix files)
   set(tmplist "")
 
@@ -19,5 +21,23 @@ function(check_bmk_processing outvar)
   endif()
 
   set(${outvar} ${hasSrcDir} PARENT_SCOPE)
+endfunction()
+
+function(create_file_cmdline_arg)
+  set(options)
+  set(oneValueArgs CMDLINE_OPTION;FILENAME;CMDLINE_ARG)
+  set(multiValueArgs)
+
+  cmake_parse_arguments(cfca "${options}" "${oneValueArgs}"
+    "${multiValueArgs}" ${ARGN})
+
+  set(${cfca_CMDLINE_ARG} "" PARENT_SCOPE)
+
+  if(NOT EXISTS ${cfca_FILENAME})
+    message(STATUS "could not find file: ${cfca_FILENAME}")
+    return()
+  endif()
+
+  set(${cfca_CMDLINE_ARG} "${cfca_CMDLINE_OPTION}=${cfca_FILENAME}" PARENT_SCOPE)
 endfunction()
 
