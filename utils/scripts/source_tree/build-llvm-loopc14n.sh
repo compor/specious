8592b558accb0b809ef9458b965702ebd4d28a15
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-04-19 19:37:03 +0100

    add simple LLVM IR bitcode pipeline

---
+++ utils/scripts/source_tree/build-llvm-loopc14n.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+PRJ_ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)"
+SRC_DIR=${1:-$PRJ_ROOT_DIR}
+INSTALL_PREFIX=${2:-../install/}
+
+BMK_CONFIG_FILE="${SRC_DIR}/configs/all_except_fortran.txt"
+
+PIPELINE_CONFIG_FILE="${SRC_DIR}/configs/pipelines/loopc14n.txt"
+
+#
+
+LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
+LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi"
+
+CC=clang CXX=clang++ \
+  cmake \
+  -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+  -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+  -DHARNESS_USE_LLVM=On \
+  -DHARNESS_BMK_CONFIG_FILE="${BMK_CONFIG_FILE}" \
+  -DHARNESS_PIPELINE_CONFIG_FILE="${PIPELINE_CONFIG_FILE}" \
+  "${SRC_DIR}"
+
